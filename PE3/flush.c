cfee401597421983a9678696e28242be9c5e048a
Nikolai Skaare <nik.skaare@gmail.com>
2022-04-07 13:43:07 +0200

    redirect completed and working

--- PE3/flush.c
+++ PE3/flush.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <stdbool.h>
 
 void parseArgs(char input[], char *args[])
 {
@@ -28,6 +29,106 @@ void printStatus(int status, char *args[])
     }
     printf("] = %d\n", status);
 }
+
+struct redirect {
+    char symbol;
+    int symidx;
+    char *before[10];
+    char *after[10];
+    char cmd[50];
+    char output[50];
+};
+
+void join(char str[], char* array[]) {
+    strcpy(str, array[0]);
+
+        int i = 1;
+        while (array[i] != NULL) {
+            strcat(str, " ");
+            strcat(str, array[i]);
+            i++;
+        }
+}
+
+struct redirect parseRedirect(char* args[]) {
+
+    struct redirect r;
+    r.symbol = '\0';
+    bzero(r.before, sizeof(r.before));
+    bzero(r.after, sizeof(r.after));
+
+    int i = 0;
+    while (args[i] != NULL)
+    {
+        if (*args[i] == '<')
+        {
+            r.symbol = '<';
+            r.symidx = i;
+        }
+        else if (*args[i] == '>')
+        {
+            r.symbol = '>';
+            r.symidx = i;
+        }
+        else if (r.symbol != '\0')
+        {
+            r.after[i - (r.symidx + 1)] = args[i];
+        }
+        else
+        {
+            r.before[i] = args[i];
+        }
+        i++;
+    }
+
+    if (r.symbol == '>') {
+        join(r.cmd, r.before);
+        join(r.output, r.after);
+    } else if (r.symbol == '<') {
+        join(r.cmd, r.after);
+        join(r.output, r.before);
+    }
+
+    return r;
+}
+
+
+int ioRedirect(char *args[])
+{
+    // iterate over args to see if it contains '<' or '>' ??
+
+    struct redirect redir = parseRedirect(args);
+
+    if (redir.symbol != '\0') {
+
+        FILE *processout;
+        FILE *filein;
+
+        if ((processout = popen(redir.cmd, "r")) == NULL) {
+            fprintf(stderr,"Error popen with %s\n", redir.cmd);
+            exit(1);
+        }
+
+        if ((filein = fopen(redir.output, "w+")) == NULL) {
+            fprintf(stderr,"Error fopen with %s\n", redir.output);
+            pclose(processout);
+            exit(1);
+        }
+
+        int ch;
+        while ((ch = fgetc(processout)) != EOF) {
+            fputc(ch, filein);
+        }
+
+        pclose(processout);
+        fclose(filein);
+
+    }     
+
+    return 0;
+}
+
+
 
 void signalHandler(int sig)
 {
@@ -68,18 +169,31 @@ int main()
         }
         else
         {
-            // fork process
-            pid_t cpid;
 
-            if ((cpid = fork()) == 0)
-            {
-                execv(cmd, args);
-                exit(0);
-            }
+            ioRedirect(args);
 
-            int status;
-            waitpid(cpid, &status, 0);
-            printStatus(status, args);
+            // // fork process
+            // pid_t cpid;
+
+            // if ((cpid = fork()) == 0)
+            // {
+            //     // filedescriptor of output, 0 -> stdout
+
+            //     if (ioRedirect(args) == 0)
+            //     {
+            //         printf("Redirect");
+            //     }
+            //     else
+            //     {
+            //         execv(cmd, args);
+            //     }
+
+            //     exit(0);
+            // }
+
+            // int status;
+            // waitpid(cpid, &status, 0);
+            // printStatus(status, args);
         }
     }
 }
